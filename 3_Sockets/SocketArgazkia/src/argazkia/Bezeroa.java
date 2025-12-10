cf3641ebcc7f2f229c5680faf000c8435b3f22ad
infaantolin <infaantolin@elorrieta-errekamari.com>
2025-12-10 23:08:53 +0100

    Socket Argazkia Zuzenduta

--- 3_Sockets/SocketArgazkia/src/argazkia/Bezeroa.java
+++ 3_Sockets/SocketArgazkia/src/argazkia/Bezeroa.java
@@ -1,16 +1,19 @@
 package argazkia;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -21,6 +24,7 @@ public class Bezeroa extends JFrame {
 	private static final int TAM_PAKETE = 4096;
 	static String fitxIzena = "error.jpg";
 	final static int PORTUA = 5000;
+	private JLabel lblArgazkia;
 
 	/**
 	 * Launch the application.
@@ -43,49 +47,94 @@ public class Bezeroa extends JFrame {
 	 */
 	public Bezeroa() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
+		lblArgazkia = new JLabel();
+		lblArgazkia.setBounds(20, 20, 350, 320);
+		add(lblArgazkia);
+
+		argazkiaJaso();
+
+		setVisible(true);
+	}
+
+	private void argazkiaJaso() {
+		final String HOST = "127.0.0.1"; // localhost
+		final int PORTUA = 5000;
+
 		try {
-			Socket bezeroa = new Socket("localhost", PORTUA);
+			Socket bezeroa = new Socket(HOST, PORTUA);
 
-			DataInputStream dos = new DataInputStream(bezeroa.getInputStream());
-			int tam = dos.readInt();
-			File fitxIrakurri = new File("kopia.jpg");
-			FileOutputStream os = new FileOutputStream(fitxIrakurri);
-			int veces = (int) tam / TAM_PAKETE;
-			int resto = (int) tam - veces * TAM_PAKETE;
-			byte[] leido = new byte[TAM_PAKETE];
+			InputStream is = bezeroa.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
 
-			for (int i = 0; i < veces; i++) {
-				dos.read(leido);
-				os.write(leido);
+			// Jasotako fitxategiaren tamaina irakurri
+			int tam = dis.readInt();
+			if (tam <= 0) {
+				erakutsiNoDisponible();
+				return;
 			}
-			if (resto != 0) {
-				dos.read(leido, 0, resto);
-				os.write(leido, 0, resto);
-			}
-			fitxIzena = "kopia.jpg";
 
-			os.close();
+			// buffer-a sortu tamaina horrekin
+			byte[] argazkiData = new byte[tam];
+
+			// Rellenar el array
+			dis.readFully(argazkiData);
+
+			// Argazkia bistaratzeko modua aukeratu/galdetu
+			bistaratzekoModuaAukeratu(argazkiData);
+			
 			bezeroa.close();
 
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Errorea:" + e.getMessage());
+			erakutsiNoDisponible();
 		}
+	}
 
-		ImageIcon fotoE = new ImageIcon(fitxIzena);
-		JLabel lblNewLabel = new JLabel(fotoE);
-		lblNewLabel.setBounds(50, 35, 308, 186);
-		contentPane.add(lblNewLabel);
+	private void bistaratzekoModuaAukeratu(byte[] argazkiData) {
+
+		String[] aukerak = { "1. Gordeta eta erakutsi (kopia.jpg)", "2. Zuzen-zuzenean erakutsi" };
+
+		int aukera = JOptionPane.showOptionDialog(this, "Aukeratu zer egin nahi duzun:", "Aukerak",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, aukerak, aukerak[0]);
+
+		if (aukera == 0) {
+			// 1 aukera: Gorde kopia bat eta erakutsi
+			try (FileOutputStream fos = new FileOutputStream("kopia.jpg")) {
+				fos.write(argazkiData);
+			} catch (Exception e) {
+				System.err.println("Errorea kopia gordetzean.");
+			}
+			erakutsiIrudiaFitxategitik("kopia.jpg");
+
+		} else {
+			// 2, aukera: erakutsi zuzenean
+			erakutsiIrudiaByte(argazkiData);
+		}
+	}
+
+	private void erakutsiIrudiaFitxategitik(String helbidea) {
+		ImageIcon icon = new ImageIcon(helbidea);
+		Image img = icon.getImage().getScaledInstance(350, 320, Image.SCALE_SMOOTH);
+		lblArgazkia.setIcon(new ImageIcon(img));
+	}
+
+	private void erakutsiIrudiaByte(byte[] datuak) {
+		ImageIcon icon = new ImageIcon(datuak);
+		Image img = icon.getImage().getScaledInstance(350, 320, Image.SCALE_SMOOTH);
+		lblArgazkia.setIcon(new ImageIcon(img));
+	}
+
+	private void erakutsiNoDisponible() {
+		ImageIcon icon = new ImageIcon("noDisponible.jpg");
+		Image img = icon.getImage().getScaledInstance(350, 320, Image.SCALE_SMOOTH);
+		lblArgazkia.setIcon(new ImageIcon(img));
 	}
 
 }
--- 3_Sockets/SocketArgazkia/src/argazkia/Zerbitzaria.java
+++ 3_Sockets/SocketArgazkia/src/argazkia/Zerbitzaria.java
@@ -1,52 +1,50 @@
 package argazkia;
 
+import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Zerbitzaria {
 
 	public static void main(String[] args) {
-		final int TAM_PAKETE = 4096;
 		final int PORTUA = 5000;
-		
+
 		try {
 			ServerSocket zerbitzaria = new ServerSocket(PORTUA);
 			System.out.println("Zerbitzaria itxoiten");
-			Socket bezeroa = zerbitzaria.accept();
-			System.out.println(bezeroa.getPort() + " portuan bezeroa konektatu da.");
 
-			DataOutputStream dos = new DataOutputStream(bezeroa.getOutputStream());
-			
-			File bidaltzekoFitx = new File("faro.jpg");
-			int tam = (int) bidaltzekoFitx.length();
-			dos.writeInt(tam);
+			while (true) {
+				Socket bezeroa = zerbitzaria.accept();
+				System.out.println(bezeroa.getPort() + " portuan bezeroa konektatu da.");
 
-			FileInputStream is = new FileInputStream(bidaltzekoFitx);
-			int zenbat = (int) tam / TAM_PAKETE;
-			int hondarra = (int) tam - zenbat * TAM_PAKETE;
-			byte[] irakurrita = new byte[TAM_PAKETE];
+				File fitxategia = new File("faro.jpg");
+				byte[] datuak = new byte[(int) fitxategia.length()];
+				
 
-			for (int i = 0; i < zenbat; i++) {
-				is.read(irakurrita);
-				dos.write(irakurrita);
+				FileInputStream fis = new FileInputStream(fitxategia);
+				BufferedInputStream bis = new BufferedInputStream(fis);
+	            bis.read(datuak, 0, datuak.length);
+
+	            DataOutputStream dos = new DataOutputStream(bezeroa.getOutputStream()); //Tamaina bidaltzeko
+	            dos.writeInt(datuak.length); // Bezeroak fitxategiaren luzeera behar duelako
+	            dos.write(datuak); // Argazkia
+	            dos.flush();
+
+	            System.out.println("Imagen enviada correctamente.");
+
+				bis.close();
+				dos.close();
+				bezeroa.close();
 			}
-			if (hondarra != 0) {
-				is.read(irakurrita, 0, hondarra);
-				dos.write(irakurrita, 0, hondarra);
-			}
-			
-			is.close();
-			dos.close();
-			bezeroa.close();
-			zerbitzaria.close();
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Errorea:" + e.getMessage());
 		}
 	}
 
